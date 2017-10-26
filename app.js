21db1d2fda16d241700ef02178b7bce15ef7d24f
Sunit Prasad 2 <spra27@sapient.com>
2017-10-26 23:52:44 +0530

    model , schema , standup UI added

--- app.js
+++ app.js
@@ -8,11 +8,27 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+var mongoose = require('mongoose');
+var options = {
+  useMongoClient: true,
+  autoIndex: false, // Don't build indexes
+  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+  reconnectInterval: 500, // Reconnect every 500ms
+  poolSize: 10, // Maintain up to 10 socket connections
+  // If not connected, return errors immediately rather than waiting for reconnect
+  bufferMaxEntries: 0
+};
+var conuri = 'mongodb://theadmin:steamer123@localhost:27017/localRepo';
+mongoose.connect(conuri,options);
+
 var app = express();
 
+//assign the ejs view engine to .html files
+var ejs = require('ejs');
+app.engine('html',ejs.renderFile);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,7 +38,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/',  index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var standupCtrl = require('../controller/standup.server.controller.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/*Get new note page */
+router.get('/newnote', function(req,res){
+   return standupCtrl.getNote(req,res); 
+}); 
+
+/*Post new note page */ 
+router.post('/newnote', function(req,res){
+  return standupCtrl.create(req,res); 
 });
 
 module.exports = router;
